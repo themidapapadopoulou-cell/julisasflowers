ddc53432004bc2d27cadb048d01cb3ad8e4313df
themidapapadopoulou-cell <themida.papadopoulou@gmail.com>
2026-03-10 22:18:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openEnvelope(){
+
+    const envelope = document.getElementById("envelope")
+
+    envelope.classList.add("open")
+
+}
+
+function showFlowers(){
+
+    document.getElementById("garden").style.display="block"
+
+}
